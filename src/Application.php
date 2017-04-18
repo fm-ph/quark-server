0a4c121caf25370b3cfa61904278136dede755cb
Fabien Motte <contact@fabienmotte.com>
2017-04-18 10:31:42 +0200

    fix(application): move twig data l10n

--- src/Application.php
+++ src/Application.php
@@ -230,11 +230,11 @@ class Application
         'name' => $currentRoute->getName(),
         'parameters' => $currentRoute->getParameters()
       ],
+      'l10n' => $this->l10n,
       'locale' => [
         'code' => $this->locale,
         'available' => (array) Locale::getAvailableLocales(),
-        'country' => $this->geocoder instanceof Geocoder ? $this->geocoder->getCountry() : config('locale.country'),
-        'l10n' => $this->l10n
+        'country' => $this->geocoder instanceof Geocoder ? $this->geocoder->getCountry() : config('locale.country')
       ],
       'browser' => $this->userAgent->getBrowser(),
       'operatingSystem' => $this->userAgent->getOperatingSystem(),
